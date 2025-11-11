66eaf3cbbfe3f7b811d8258d0a836603820c3826
TheShadowless <kittipom.ku@gmail.com>
2025-11-11 20:42:57 +0700

    Sound
    edit some code and add sound file

--- Assets/Pao/GamePlayPao.cs
+++ Assets/Pao/GamePlayPao.cs
@@ -1,8 +1,9 @@
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class GamePlayPao : MonoBehaviour
-{
+{   
+    //stuctor
     [SerializeField] private GameObject[] Cube;
     [SerializeField] private GameObject SpawnPoint;
     [SerializeField] public GameObject Cam;
@@ -14,15 +15,42 @@ public class GamePlayPao : MonoBehaviour
     public float canSpawm = 5;
     int random;
     public int life;
-     private void Start()
-     {
+
+    [Header("=== Audio Sources ===")]
+    public AudioSource SFXSource;
+    public AudioSource MusicSource;
+
+    [Header("=== SFX Clips ===")]
+    public AudioClip sfxSpawn;
+    public AudioClip sfxImpact;
+    public AudioClip sfxLifeLost;
+    public AudioClip sfxWin;
+    public AudioClip sfxLose;
+
+    [Header("=== BGM ===")]
+    public AudioClip bgmLoop;
+
+    //=========================
+
+    private void Start()
+    {
+        if (MusicSource && bgmLoop)
+        {
+            MusicSource.clip = bgmLoop;
+            MusicSource.loop = true;
+            MusicSource.Play();
+        }
+
         Instantiate(Cube[0], SpawnPoint.transform.position, Quaternion.identity);
         cubeHeight = Cube[0].transform.localScale.y;
         hascube = true;
         isMoving = false;
         life = 3;
         Debug.Log(life);
+
+        PlaySFX(sfxSpawn);
     }
+
     void Update()
     {
         if (!hascube)
@@ -33,21 +61,46 @@ public class GamePlayPao : MonoBehaviour
             hascube = true;
             isMoving = false;
             time = 0;
+
+            PlaySFX(sfxSpawn);
         }
 
         if ((Input.GetMouseButtonDown(0) && hascube) || (Input.touchCount > 0 && hascube))
         {
             isMoving = true;
-            cubecount ++;
+            cubecount++;
+            //fall sound 
         }
 
-        if(cubecount >= 10)
+        if (cubecount >= 10)
         {
             Debug.Log("win");
+            PlaySFX(sfxWin);
         }
-        if(life <= 0)
+        if (life <= 0)
         {
             Debug.Log("lose");
+            PlaySFX(sfxLose);
         }
+    }
+
+    public void PlaySFX(AudioClip clip, float volume = 1f)
+    {
+        if (clip != null && SFXSource != null)
+        {
+            SFXSource.PlayOneShot(clip, volume);
+        }
+    }
+
+    public void OnCubeImpact(bool isPerfect)
+    {
+        if (SFXSource && sfxImpact)
+            PlaySFX(sfxImpact);
+    }
+
+    public void OnLifeLost()
+    {
+        if (SFXSource && sfxLifeLost)
+            PlaySFX(sfxLifeLost);
     }
 }
