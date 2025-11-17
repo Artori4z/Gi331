97eba92d314908cbc4556da7c026dc88e714088e
Kongphop <kongphop.buch@bumail.net>
2025-11-17 20:42:41 +0700

    0.6

--- Assets/Pao/Cube.cs
+++ Assets/Pao/Cube.cs
@@ -2,18 +2,21 @@
 
 public class Cube : MonoBehaviour
 {
-
-    public float speedDown = Physics.gravity.y;
     public GamePlayPao gamePlay;
     Rigidbody rb;
-    public Transform startPoint;   // จุดเริ่มทางซ้าย
-    public Transform midPoint;     // จุดต่ำสุด (กลาง)
-    public Transform endPoint;     // จุดทางขวา
-    public float speed = 1f;       // ความเร็วการเคลื่อนที่
+    public Transform startPoint;
+    public Transform midPoint;
+    public Transform endPoint;
     private float t = 0f;
     public float timeDelay = 0f;
     public bool goingToStart = true;
     public bool hit = false;
+    private bool initialized = false;
+    public float speed = 2;
+    private enum StartType { Start, Mid, End }
+    private StartType startType;
+    private int phase = 0;
+    Vector3 position;
     [System.Obsolete]
     private void Start()
     {
@@ -25,27 +28,118 @@ public class Cube : MonoBehaviour
     }
     void Update()
     {
-
-        if (gamePlay.isMoving == true)
+        switch (gamePlay.Lv)
         {
-            rb.useGravity = true;
-            rb.AddForce(Physics.gravity * 0.5f, ForceMode.Acceleration);
+            case 1:
+                speed = 1f;
+                break;
+            case 2:
+                speed = 1.5f;
+                break;
+            case 3:
+                speed = 2f;
+                break;
         }
-        if (!hit && gamePlay.isMoving == false)
+        if (gamePlay.isMoving)
         {
-            t += Time.deltaTime * speed;
-            rb.useGravity = false;
-            Vector3 position =
-                Mathf.Pow(1 - t, 2) * startPoint.position +
-                2 * (1 - t) * t * midPoint.position +
-                Mathf.Pow(t, 2) * endPoint.position;
-
-            transform.position = position;
-
-            if (t > 1f)
+            if (gamePlay.boots)
             {
-                (startPoint, endPoint) = (endPoint, startPoint);
-                t = 0f;
+                rb.useGravity = true;
+                rb.AddForce(Physics.gravity * 100, ForceMode.Acceleration);
+            }
+            rb.useGravity = true;
+            rb.AddForce(Physics.gravity * 10, ForceMode.Acceleration);
+        }
+        if (!gamePlay.isMoving)
+        {
+            rb.useGravity = false;
+        }
+        if (!hit && !gamePlay.isMoving && !gamePlay.boots)
+        {
+            if (!initialized)
+            {
+                if (gamePlay.Lv > 3)
+                {
+                    speed = gamePlay.EndlessSpeed;
+                }
+                int randomStart = gamePlay.randomSpawn;
+                switch (randomStart)
+                {
+                    case 1:
+                        startType = StartType.Start;
+                        transform.position = startPoint.position;
+                        phase = 0;
+                        break;
+                    case 0:
+                        startType = StartType.Mid;
+                        transform.position = midPoint.position;
+                        phase = 0;
+                        break;
+                    case 2:
+                        startType = StartType.End;
+                        transform.position = endPoint.position;
+                        phase = 0;
+                        break;
+                }
+                initialized = true;
+            }
+            switch (startType)
+            {
+                case StartType.Start:
+                    t += Time.deltaTime * speed;
+                    rb.useGravity = false; 
+                    position = Mathf.Pow(1 - t, 2) 
+                        * startPoint.position + 2 * (1 - t) * t 
+                        * midPoint.position + Mathf.Pow(t, 2) 
+                        * endPoint.position;
+                    transform.position = position; 
+                    if (t > 1f)
+                    {
+                        (startPoint, endPoint) = (endPoint, startPoint);
+                        t = 0f; 
+                    }
+                    break;
+                case StartType.Mid:
+                    if (phase == 0)
+                    {
+                        t += Time.deltaTime * (speed*2);
+                        Vector3 position =
+                            Mathf.Pow(1 - t, 2) * midPoint.position +
+                            2 * (1 - t) * t * ((midPoint.position + endPoint.position) / 2) +
+                            Mathf.Pow(t, 2) * endPoint.position;
+                        transform.position = position;
+                        if (t >= 1f) { t = 0f; phase = 1; }
+                    }
+                    else if (phase == 1)
+                    {
+                        t += Time.deltaTime * speed;
+                        rb.useGravity = false;
+                        position = Mathf.Pow(1 - t, 2)
+                            * endPoint.position + 2 * (1 - t) * t
+                            * midPoint.position + Mathf.Pow(t, 2)
+                            * startPoint.position;
+                        transform.position = position;
+                        if (t > 1f)
+                        {
+                            (startPoint, endPoint) = (endPoint, startPoint);
+                            t = 0f;
+                        }
+                    }
+                    break;
+                case StartType.End:
+                    t += Time.deltaTime * speed;
+                    rb.useGravity = false;
+                    position = Mathf.Pow(1 - t, 2)
+                        * endPoint.position + 2 * (1 - t) * t
+                        * midPoint.position + Mathf.Pow(t, 2)
+                        * startPoint.position;
+                    transform.position = position;
+                    if (t > 1f)
+                    {
+                        (startPoint, endPoint) = (endPoint, startPoint);
+                        t = 0f;
+                    }
+                    break;
             }
         }
         if (hit)
@@ -54,43 +148,82 @@ public class Cube : MonoBehaviour
             {
                 gamePlay.isMoving = false;
             }
-
-            float zRot = transform.eulerAngles.z;
-            // แปลงให้ช่วงอยู่ระหว่าง -180 ถึง 180 (จะได้ไม่ต้องกังวลค่า 359)
-            if (zRot > 180f) zRot -= 360f;
-
-            if (Mathf.Abs(zRot) <= 1f)
+            if (!gamePlay.boots)
             {
-                timeDelay += Time.deltaTime;
-
-                if (timeDelay >= 2f)
+                if (gamePlay.perfectCount == 9)
                 {
-                    gamePlay.hascube = false;
-                    gamePlay.Cam.transform.position += new Vector3(0, gamePlay.cubeHeight, 0);
-                    Destroy(rb);
-                    Destroy(this);
-                    hit = false;
-                    gamePlay.cubecount++;
+                    timeDelay += Time.deltaTime;
+                    if (timeDelay >= 1f)
+                    {
+                        this.transform.rotation = Quaternion.Euler(0, 0, 0);
+                        this.transform.position = new Vector3(0, this.transform.position.y, this.transform.position.z);
+                        if (this.transform.position == new Vector3(0, this.transform.position.y, this.transform.position.z))
+                        {
+                            gamePlay.hascube = false;
+                            gamePlay.Cam.transform.position += new Vector3(0, gamePlay.cubeHeight, 0);
+                            Destroy(rb);
+                            Destroy(this);
+                            hit = false;
+                            gamePlay.cubecount++;
+                            gamePlay.perfectCount++;
+                            if (gamePlay.Lv > 3)
+                            {
+                                gamePlay.EndlessSpeed += 0.1f;
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    float zRot = transform.eulerAngles.z;
+                    if (zRot > 180f) zRot -= 360f;
+                    timeDelay += Time.deltaTime;
+                    if (Mathf.Abs(zRot) <= 10f)
+                    {
+                        if (timeDelay >= 1f)
+                        {
+                            gamePlay.hascube = false;
+                            gamePlay.Cam.transform.position += new Vector3(0, gamePlay.cubeHeight, 0);
+                            Destroy(rb);
+                            Destroy(this);
+                            hit = false;
+                            gamePlay.cubecount++;
+                            gamePlay.perfectCount++;
+                            if (gamePlay.Lv > 3)
+                            {
+                                gamePlay.EndlessSpeed += 0.1f;
+                            }
+                        }
+                    }
                 }
             }
-            else
+            else if (gamePlay.boots)
             {
-                timeDelay = 0f;
+                gamePlay.hascube = false;
+                gamePlay.Cam.transform.position += new Vector3(0, gamePlay.cubeHeight, 0);
+                Destroy(rb);
+                Destroy(this);
+                hit = false;
+                gamePlay.cubecount++;
+                rb.useGravity = false;
             }
         }
     }
     private void OnCollisionEnter(Collision collision)
     {
-        // ถ้า tag ของวัตถุที่ชนคือ "Cube"
         if (collision.gameObject.CompareTag("Cube"))
         {
-            hit = true;
-            if (collision.transform.position.x == this.transform.position.x)
+            if (gamePlay.Lv2Swap)
             {
-                Debug.Log("Perfect");
+                Debug.Log("true");
+                gamePlay.Lv2Swap = false;
+            }else
+            {
+                Debug.Log("false");
+                gamePlay.Lv2Swap = true;
             }
+            hit = true;
         }
-        // ถ้า tag ของวัตถุที่ชนคือ "Lose"
         else if (collision.gameObject.CompareTag("Lose"))
         {
             if (gamePlay != null)
@@ -99,9 +232,17 @@ public class Cube : MonoBehaviour
             }
             Destroy(this.gameObject);
             gamePlay.life -= 1;
-            Debug.Log(gamePlay.life);
             gamePlay.hascube = false;
+            gamePlay.perfectCount = 0;
+        }
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Cube"))
+        {
+            hit = false;
+            gamePlay.isMoving = true;
+            timeDelay = 0;
         }
     }
 }
-
--- Assets/Pao/GamePlayPao.cs
+++ Assets/Pao/GamePlayPao.cs
@@ -1,52 +1,133 @@
+﻿using System.Net;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class GamePlayPao : MonoBehaviour
 {
     [SerializeField] private GameObject[] Cube;
-    [SerializeField] private GameObject SpawnPoint;
+    [SerializeField] private GameObject[] SpawnPoint;
     [SerializeField] public GameObject Cam;
+    [SerializeField] public GameObject[] LvTwo;
     public int cubecount = 0;
     public float cubeHeight;
     public bool isMoving = false;
     public bool hascube = false;
-    public float time = 5;
     public float canSpawm = 5;
     int random;
     public int life;
-     private void Start()
+    public int randomSpawn = 0;
+    public int Lv = 3;
+    public int perfectCount = 0;
+    public float EndlessSpeed = 2;
+    public bool Lv2Swap;
+    private bool moveUpOne = true;
+    private bool moveUpTwo = true;
+    private float fixedZ;
+    public float bootsTimer = 0f;
+    public bool boots;
+    public Material[] Sky;
+    private void Start()
      {
-        Instantiate(Cube[0], SpawnPoint.transform.position, Quaternion.identity);
+        Instantiate(Cube[0], SpawnPoint[randomSpawn].transform.position, Quaternion.identity);
         cubeHeight = Cube[0].transform.localScale.y;
         hascube = true;
         isMoving = false;
         life = 3;
-        Debug.Log(life);
+        boots = false;
+        fixedZ = SpawnPoint[1].transform.localPosition.z;
+        RenderSettings.skybox = Sky[0];
+        DynamicGI.UpdateEnvironment();
     }
     void Update()
     {
-        if (!hascube)
+        if (perfectCount == 10)
         {
-            random = Random.Range(0, Cube.Length);
-            cubeHeight = Cube[random].transform.localScale.y;
-            Instantiate(Cube[random], SpawnPoint.transform.position, Quaternion.identity);
-            hascube = true;
-            isMoving = false;
-            time = 0;
+            boots = true;
         }
-
-        if ((Input.GetMouseButtonDown(0) && hascube) || (Input.touchCount > 0 && hascube))
+        if (bootsTimer >= 5)
+        {
+            boots = false;
+            perfectCount = 0;
+            bootsTimer = 0;
+        }
+        if (boots)
+        {
+             bootsTimer += Time.deltaTime;
+            if (!hascube)
+                {
+                    Instantiate(Cube[0], SpawnPoint[0].transform.position, Quaternion.identity);
+                    cubeHeight = Cube[random].transform.localScale.y;
+                    hascube = true;
+                    isMoving = false;  
+                }
+        } 
+        else
+        {
+            if (!hascube)
+            {
+                    random = Random.Range(0, Cube.Length);
+                    randomSpawn = Random.Range(0, SpawnPoint.Length);
+                    cubeHeight = Cube[random].transform.localScale.y;
+                    Instantiate(Cube[random], SpawnPoint[randomSpawn].transform.position, Quaternion.identity);
+                    hascube = true;
+                    isMoving = false;
+            }
+        }
+        if ((Keyboard.current.spaceKey.isPressed && hascube) || (Input.touchCount > 0 && hascube))
         {
             isMoving = true;
         }
-
-        if(cubecount >= 10)
+        switch (cubecount)
         {
-            Debug.Log("win");
+            case 10:
+                Lv = 2;
+                break;
+            case 15:
+                Lv = 3;
+                break;
+            case 20:
+                Lv = 4;
+                break;
         }
-        if(life <= 0)
+        if (Lv == 2)
         {
-            Debug.Log("lose");
+            RenderSettings.skybox = Sky[1];
+            DynamicGI.UpdateEnvironment();
+            if (Lv2Swap)
+            {
+                SpawnPoint[0].transform.position = LvTwo[0].transform.position;
+                SpawnPoint[1].transform.position = LvTwo[3].transform.position;
+                SpawnPoint[2].transform.position = LvTwo[4].transform.position;
+            }
+            else
+            {
+                SpawnPoint[0].transform.position = LvTwo[0].transform.position;
+                SpawnPoint[1].transform.position = LvTwo[1].transform.position;
+                SpawnPoint[2].transform.position = LvTwo[2].transform.position;
+            }
+        }else if (Lv >= 3)
+        {
+            RenderSettings.skybox = Sky[2];
+            DynamicGI.UpdateEnvironment();
+            MoveSpawnPointUpDown(SpawnPoint[1], ref moveUpOne, fixedZ, LvTwo[1], LvTwo[3]);
+            MoveSpawnPointUpDown(SpawnPoint[2], ref moveUpTwo, fixedZ, LvTwo[4], LvTwo[2]);          
         }
+        if (life <= 0)
+        {
+            Time.timeScale = 0f;
+        }
+    }
+    private void MoveSpawnPointUpDown(GameObject spawn, ref bool moveUp, float fixedLocalZ, GameObject minPoint, GameObject maxPoint)
+    {
+        Transform sp = spawn.transform;
+        Vector3 localPos = sp.localPosition;
+        if (localPos.y >= maxPoint.transform.localPosition.y)
+            moveUp = false;
+        else if (localPos.y <= minPoint.transform.localPosition.y)
+            moveUp = true;
+        float direction = moveUp ? 1f : -1f;
+        localPos.y += direction * 2f * Time.deltaTime;
+        localPos.z = fixedLocalZ;
+        sp.localPosition = localPos;
     }
 }
